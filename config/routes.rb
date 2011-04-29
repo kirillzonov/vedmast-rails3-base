724bb913dd90ed5f07d9dbdc6661f91ad0b33557
kirillzonov <kirillzonov@flatsoft.com>
2011-04-29 22:53:48 +0400

    removed oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails3Base::Application.routes.draw do
   resources :articles
 
-  devise_for :users,
-      :controllers => {:omniauth_callbacks => 'omniauth_callbacks'}
+  devise_for :users
 
   as :user do
     get 'login', :to => 'devise/sessions#new', :as => 'new_user_session'
     get 'logout', :to => 'devise/sessions#destroy', :as => 'destroy_user_session'
-    get 'signup', :to => 'devise/registrations#new', :as => 'new_user_registration'
   end
 
   root :to => 'dashboard#index'
