b8237f271bbd47c548cedf3a12059080515a123c
kirillzonov <kirillzonov@flatsoft.com>
2011-04-30 16:06:51 +0400

    get_categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
+  helper_method :get_categories
+  before_filter :get_categories
+  before_filter :get_new_articles
   protect_from_forgery
   layout 'application'
 
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
+  end
+
+  def get_new_articles
+    @new_articles = Article.recent
+  end
+
+  def get_categories
+    @categories = Category.all
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
   belongs_to :category
+  scope :recent, order("created_at desc").limit(5)
 end
