d4fc57cf865d1afe7cb810e1f9515da98562a0a1
kirillzonov <kirillzonov@flatsoft.com>
2011-04-30 12:11:14 +0400

    removed omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails3Base::Application.routes.draw do
   as :user do
     get 'login', :to => 'devise/sessions#new', :as => 'new_user_session'
     get 'logout', :to => 'devise/sessions#destroy', :as => 'destroy_user_session'
+    get 'signup', :to => 'devise/registrations#new', :as => 'new_user_registration'
   end
 
   root :to => 'dashboard#index'
