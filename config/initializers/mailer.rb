4d3231ae383a9a9b8c8aa9cb3df2bce809e55a9b
kirillzonov <kirillzonov@flatsoft.com>
2011-05-02 14:25:27 +0400

    make email sending work properly

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,1 +1,10 @@
 ActionMailer::Base.default_url_options[:host] = configatron.host
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address        => 'smtp.gmail.com',
+    :port           => 587,
+    :authentication => :plain,
+    :user_name      => 'drevnmast@gmail.com',
+    :password       => 'vfcrvfcr'
+  }
