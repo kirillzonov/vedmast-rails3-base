d8f5ab5f0c5cd388658a2c80e815691d2c7305e9
graffzon <graffzon@gmail.com>
2011-06-13 11:07:04 +0400

    added default scope for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ActiveRecord::Base
   has_and_belongs_to_many :orders, :join_table => "articles_orders", :association_foreign_key => 'order_id'
   paginates_per 4
   scope :recent, order("created_at desc").limit(5)
+  default_scope order('created_at desc')
 end
